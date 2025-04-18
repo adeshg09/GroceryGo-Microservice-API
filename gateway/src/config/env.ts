f230aa4d7d693b03d784890984f57216d8350541
adesh09 <adeshgadage0908@gmail.com>
2025-04-19 01:40:24 +0530

    gateway env.ts fix

--- gateway/src/config/env.ts
+++ gateway/src/config/env.ts
@@ -4,5 +4,6 @@ dotenv.config();
 export const env = {
   GATEWAY_PORT: process.env.PORT || 4000,
   AUTH_SERVICE_PORT: process.env.AUTH_SERVICE_PORT || 3001,
-  AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || "http://localhost:3001",
+  // AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || "http://localhost:3001",
+  AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL,
 };
